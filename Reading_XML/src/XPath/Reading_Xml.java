78e47d494d9ca792536a4541be2932319be4d9a4
Viji-P <mpvijayalakshmi91103@gmail.com>
2023-09-01 20:21:03 +0530

    update

--- Reading_XML/src/XPath/Reading_Xml.java
+++ Reading_XML/src/XPath/Reading_Xml.java
@@ -1,46 +1,71 @@
 package XPath;
 
-import javax.xml.parsers.*;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
-import javax.xml.xpath.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+
 
 public class Reading_Xml {
 
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		Reading_Xml rx=new Reading_Xml();
-		rx.display();
 		
 		
-		
-
-	}
-	public void display() throws Exception {
-		DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
-		
-		DocumentBuilder db=dbf.newDocumentBuilder();
-		
-		Document d=db.parse("src//Movies.xml");
-		
-		d.getDocumentElement().normalize();
-		
-		XPathFactory xp=XPathFactory.newInstance();
-		
-		XPath x=xp.newXPath();
-		
-		String expression="//movie/name";
-		
-		NodeList nl=(NodeList) x.compile(expression).evaluate(d, XPathConstants.NODESET);
-		
-		for(int i=0;i<nl.getLength();i++) {
-			Node n=nl.item(i);
-			System.out.println("Title  :"+n.getTextContent());
+		Reading_Xml read=new Reading_Xml();
+		read.display();
 		}
 		
 		
+		public void display() throws Exception {
+		Map<String,String> dm=new HashMap<>();
 		
-	}
-
+		String filePath="src//Movies.xml";
+		
+		DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+		
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		Document document=builder.parse(new File(filePath));
+		
+		//d.getDocumentElement().normalize();
+		
+		XPathFactory xPathFactory = XPathFactory.newInstance();
+		
+		XPath xpath = xPathFactory.newXPath();
+		
+		
+		
+		String expression = "//movie[1]/name";
+		
+        
+        NodeList nl=(NodeList) xpath.compile(expression).evaluate(document, XPathConstants.NODESET);
+        
+        for(int i=0;i<nl.getLength();i++) {
+        	String elementName=nl.item(i).getNodeName();
+        	String elementValue=nl.item(i).getTextContent();
+        	
+            dm.put(elementName, elementValue);
+        	
+        }
+		
+		
+        for (Map.Entry<String, String> entry : dm.entrySet()) 
+    		System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+    
+	
+		}
 }
+	
+	
+
+
