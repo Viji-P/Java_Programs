c363e95cf13e7f7ec0bd59a30481d3eb52f6c146
Viji-P <mpvijayalakshmi91103@gmail.com>
2023-09-05 19:16:13 +0530

    some change to read xml

--- Reading_XML/src/XPath/Reading_Xml.java
+++ Reading_XML/src/XPath/Reading_Xml.java
@@ -1,6 +1,6 @@
 package XPath;
 
-import java.io.File;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,7 +8,7 @@ import java.util.Map;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-
+import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,11 +23,14 @@ public class Reading_Xml {
 
 	public static void main(String[] args) throws Exception{
 		
-		
 		Reading_Xml read=new Reading_Xml();
 		
-		String filePath="src//xml_Files";
-		List<Map<String,String>> result=read.readXmlFiles(filePath);
+		String[] xmlStrings= {
+				"<fruits><fruit><name>Apple</name></fruit></fruits>",
+				"<bookstore><book><title>The Hobbit</title></book></bookstore>"
+				};
+		
+		List<Map<String,String>> result=read.readXmlFiles(xmlStrings);
 		
 		System.out.println(result);
 		
@@ -35,19 +38,17 @@ public class Reading_Xml {
 		}
 		
 		
-		public List<Map<String,String>> readXmlFiles(String filePath) throws Exception {
+		public List<Map<String,String>> readXmlFiles(String[] xmlStrings) throws Exception {
 		List<Map<String,String>> dm=new ArrayList<>();
 		
-		File directory=new File(filePath);
-		File[] files=directory.listFiles((dir,name) -> name.toLowerCase().endsWith(".xml"));
 		
 		DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
 		
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		
-		for(File file:files) {
+		for(String String:xmlStrings) {
 		
-		Document document=builder.parse(file);
+		Document document=builder.parse(new InputSource(new StringReader(String)));
 		
 		document.getDocumentElement().normalize();
 		
@@ -57,12 +58,13 @@ public class Reading_Xml {
 		
 		
 		
-		String expression = "//movies";
+		String expression = "//fruits";
 		
         
         NodeList nl=(NodeList) xpath.compile(expression).evaluate(document, XPathConstants.NODESET);
         
         for(int i=0;i<nl.getLength();i++) {
+        	
         	String elementName=nl.item(i).getNodeName();
         	String elementValue=nl.item(i).getTextContent();
         	
