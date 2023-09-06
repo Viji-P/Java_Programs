0a5a6414e553f364f4dd0a9318a0ba6ff3f341dd
Viji-P <mpvijayalakshmi91103@gmail.com>
2023-09-06 17:22:34 +0530

    some modified

--- Reading_XML/src/XPath/ReadingXml.java
+++ Reading_XML/src/XPath/ReadingXml.java
@@ -1,6 +1,7 @@
 package XPath;
 
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +13,12 @@ import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 
 public class ReadingXml {
@@ -23,15 +30,28 @@ public class ReadingXml {
 		
 		String xmlStrings= "<bookstore><book><title>The Hobbit</title>"
 				+ "<price>12.99</price></book></bookstore>";
-				
 		
-		List<Map<String,String>> result=read.readXmlFiles(xmlStrings);
-		
-		System.out.println(result);
+		String xmlToString=read.convertToString(xmlStrings);
+		System.out.println(xmlToString);
 		
 		
 		}
 		
+		public String convertToString(String xmlStrings) throws Exception {
+			
+			TransformerFactory transformFactory=TransformerFactory.newInstance();
+			Transformer transformer=transformFactory.newTransformer();
+			StringWriter writer=new StringWriter();
+			DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+			
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+		    Document doc=builder.parse(new InputSource(new StringReader(xmlStrings)));	
+		    
+		    transformer.transform(new DOMSource(doc), new StreamResult(writer));
+			return writer.toString();
+		}
+	
 		
 		public List<Map<String,String>> readXmlFiles(String xmlStrings) throws Exception {
 		List<Map<String,String>> dm=new ArrayList<>();
